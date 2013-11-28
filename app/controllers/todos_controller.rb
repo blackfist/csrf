6bed8a2a3c6349f8b85719dc9edf2b8da1efea88
blackfist <blackfist@vm-0.blackfist.koding.kd.io>
2013-11-28 05:13:51 +0000

    styles up the new todo form and adds flash message support.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -30,7 +30,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to todos_path, notice: 'Todo was successfully created.' }
         format.json { render action: 'show', status: :created, location: @todo }
       else
         format.html { render action: 'new' }
